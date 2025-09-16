805ed2a7560c6dfced40677b5f8dfab9986df75e
2saB <pthusaba@macalester.edu>
2025-09-16 11:19:59 -0500

    commit

---
+++ src/ica/04-functions/my_avg.py
@@ -0,0 +1,6 @@
+def ave3(x, y, z):
+    res = (x + y + z) / 3.0
+    return res
+
+res = ave3(2, 3.0, 4.0)
+print(res)
---
+++ src/ica/04-functions/my_print.py
@@ -0,0 +1,2 @@
+def print_name(""):
+    print('Welcome to COMP 123'+ print_name)
