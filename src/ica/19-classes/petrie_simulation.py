fc0b112549141c3d4e855431f72b2e87f0511da4
2saB <pthusaba@macalester.edu>
2025-11-03 10:37:21 -0600

    commit

--- src/ica/19-classes/petrie_simulation.py
+++ src/ica/19-classes/petrie_simulation.py
@@ -19,8 +19,11 @@ class Employee:
         saves those values to instance variables. It also initializes the
         variable that holds the comments received by this employee to zero.
         """
-        # TODO: Implement this method then remove this line
-        pass
+
+        self.gender = gender
+        self.will_comment = will_comment
+        self.comments_received = 0
+
 
     def __str__(self):
         """
@@ -31,14 +34,29 @@ class Employee:
                 + str(self.comments_received)
                 + " sexist comments received")
 
+    def set_commenter_status(self, will_comment):
+        self.will_comment = will_comment
+
+    def receive_sexist_comment(self):
+        self.comments_received += 1
+
+    def get_gender(self):
+        return self.gender
+
+    def get_commenter_status(self):
+        return self.will_comment
+
+    def get_comments_received(self):
+        return self.comments_received
+
 
 def print_employee_list(lst):
     """
     Given a list of employees, this method will print the details of each employee
     by using the print() method
     """
-    # TODO: Implement this function then remove this line
-    pass
+    for employee in lst:
+        print(employee)
 
 
 def create_employees(total_num):
@@ -46,8 +64,23 @@ def create_employees(total_num):
     Takes in the number of employees to make, builds and returns a list that contains
     that many employees. It ensures that ~80% are men and the rest women.
     """
-    # TODO: Implement this function then remove this line
-    pass
+
+    employees = []
+
+    # Calculate number of men (80%) and women (remaining)
+    num_men = math.floor(total_num * 0.8)
+    num_women = total_num - num_men
+
+    # male employees
+    for employee in range(num_men):
+        employees.append(Employee("Man", False))
+
+    # female employees
+    for employee in range(num_women):
+        employees.append(Employee("Woman", False))
+
+    return employees
+
 
 
 def create_commenters(lst):
@@ -55,8 +88,11 @@ def create_commenters(lst):
     Given a list of employees, make 20% of each gender be sexist employees. This
     method should not return anything.
     """
-    # TODO: Implement this function then remove this line
-    pass
+
+    for employee in lst:
+
+        if random.random() < 0.2:            # generates a random value between 0 and 1
+            employee.set_commenter_status(True)                        # 20% chance the employee becomes a commenter
 
 
 def generate_comments(lst):
@@ -65,8 +101,20 @@ def generate_comments(lst):
     another employee of the opposite gender, chosen randomly. This method should
     not return anything
     """
-    # TODO: Implement this function then remove this line
-    pass
+
+    # Split employees by gender
+    men = [emp for emp in lst if emp.get_gender() == "Man"]
+    women = [emp for emp in lst if emp.get_gender() == "Woman"]
+
+    for man in men:
+        if man.get_commenter_status():
+            recipient = random.choice(women)           #chooses a random woman
+            recipient.receive_sexist_comment()
+
+    for woman in women:
+        if woman.get_commenter_status():                     #chooses a random man
+            recipient = random.choice(men)
+            recipient.receive_sexist_comment()
 
 
 def average_comments(lst):
@@ -74,8 +122,21 @@ def average_comments(lst):
     Returns a tuple that represents the average amount of comments received for men and women
     respectively. Return statement will be in the form (<avg_for_men>, <avg_for_women>)
     """
-    # TODO: Implement this function then remove this line
-    pass
+
+    men_comments = []
+    women_comments = []
+
+    for employee in lst:
+        if employee.get_gender() == "Man":
+            men_comments.append(employee.get_comments_received())
+        else:
+            women_comments.append(employee.get_comments_received())
+
+    men_avg = sum(men_comments) / len(men_comments) if men_comments else 0           #avoiding dividing by 0 just in case to avoid errors
+    women_avg = sum(women_comments) / len(women_comments) if women_comments else 0
+
+    return (men_avg, women_avg)
+
 
 
 def main():
@@ -120,4 +181,4 @@ if __name__ == "__main__":
     print(average_comments(employees))
 
     "<----- Run the simulation ----->"
-    # main()  # <-- KEEP THIS, Uncomment it after implementing all the functions
+    main()  # <-- KEEP THIS, Uncomment it after implementing all the functions
